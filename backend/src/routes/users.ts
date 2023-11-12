3310d477ecfa5658b5a568a5d1bdd8ac1bca1c90
Artyom Anatolьevich <kshart@yandex.ru>
2023-11-12 17:44:54 +0000

    wip

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,15 +1,22 @@
 import { FastifyInstance } from 'fastify'
 
 export default (fastify: FastifyInstance, opts: any, done: any) => {
-  fastify.get('/index', async (request, reply) => {
-    const users = await fastify.prisma.user.findMany({
-      // where: { published: true },
-      // include: { author: true },
-    })
-    return users
-  })
-  fastify.get('/sync', async (request, reply) => {
-    return request.user
-  })
+  // fastify.get('/create', async (request, reply) => {
+  //   const users = await fastify.prisma.user.findMany({
+  //     // where: { published: true },
+  //     // include: { author: true },
+  //   })
+  //   return users
+  // })
+  // fastify.get('/create', async (request, reply) => {
+  //   const users = await fastify.prisma.user.findMany({
+  //     // where: { published: true },
+  //     // include: { author: true },
+  //   })
+  //   return users
+  // })
+  // fastify.get('/sync', async (request, reply) => {
+  //   return request.user
+  // })
   done()
 }
